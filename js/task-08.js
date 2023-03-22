0f1ef89320bf9ebdd10f036ed039cf7e3ced6520
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-03-22 22:29:59 +0200

    Home Work 6

---
+++ js/task-08.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = loginForm.elements.email.value;
+    const password = loginForm.elements.password.value;
+    
+    if (!email || !password) {
+        alert('All fields must be filled');
+        return;
+    };
+
+    const loginData = {
+        email,
+        password
+    };
+
+    console.log(loginData);
+    loginForm.reset();
+});
+
+
+
+
+
+
+
